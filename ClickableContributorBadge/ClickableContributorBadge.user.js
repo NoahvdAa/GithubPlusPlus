114285e89c5df4388c0e99638d1cc667343a86a1
Noah van der Aa <ndvdaa@gmail.com>
2021-09-24 13:53:28 +0200

    Make clickable contributor badges work upon page navigation and dynamically added comments

--- ClickableContributorBadge/ClickableContributorBadge.user.js
+++ ClickableContributorBadge/ClickableContributorBadge.user.js
@@ -1,27 +1,31 @@
 // ==UserScript==
 // @name         GH++ | Clickable Contributor Badges
 // @namespace    https://github.com/NoahvdAa/GithubPlusPlus
-// @version      1.0.0
+// @version      1.1.0
 // @description  Clickable Contributor Badges makes the contributor badges in pull requests and issues clickable. When clicked, the badge will redirect you to the contributions the user made.
 // @author       NoahvdAa
 // @match        https://github.com/*
 // @icon         https://www.google.com/s2/favicons?domain=github.com
-// @downloadURL  https://github.com/NoahvdAa/GithubPlusPlus/raw/master/ClickableContributorBadge/ClickableContributorBadge.user.js
 // @grant        none
+// @require      https://cdnjs.cloudflare.com/ajax/libs/arrive/2.4.1/arrive.min.js
+// @updateURL    https://github.com/NoahvdAa/GithubPlusPlus/raw/master/ClickableContributorBadge/ClickableContributorBadge.user.js
 // ==/UserScript==
 
 (function () {
 	'use strict';
 
-	Array.from(document.querySelectorAll('.timeline-comment-label')).forEach(label => {
-		if (label.innerText !== 'Contributor') return;
+	document.arrive('.timeline-comment-label', apply);
+	Array.from(document.querySelectorAll('.timeline-comment-label')).forEach(apply);
+
+	function apply(label) {
+		if (label.innerText !== 'Contributor' || label.ghpp_clickable) return;
+		label.ghpp_clickable = true;
 		label.style.cursor = 'pointer';
 		label.onclick = function () {
 			const repoBaseURL = document.querySelector('[data-pjax="#js-repo-pjax-container"]').href;
 			const contributor = label.parentElement.parentElement.querySelector('.author').innerText;
 
-			console.log(label.parentElement.parentElement);
 			window.location = `${repoBaseURL}/commits?author=${contributor}`;
 		};
-	});
+	}
 })();
